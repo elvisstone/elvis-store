541051d349ff1f42ebd4a5157ebebeb6f1651fe7
elvisstone <etas1409@gmail.com>
2024-06-01 16:18:50 +0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import About from "../pages/About";
+import NotFound from "../pages/NotFound";
+import PrivateRouter from "./PrivateRouter";
+import ProductDetail from "../pages/ProductDetail";
+
+const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<PrivateRouter />}>
+          <Route path="" element={<Home />} />
+          <Route path="/dashboard/products" element={<Products />} />
+          <Route path="/dashboard/products/:id" element={<ProductDetail />} />
+          <Route path="/dashboard/about" element={<About />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRouter;
