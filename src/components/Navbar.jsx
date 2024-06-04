f4f93fdfdc53d942d91846ce88ad2a7dbb4c4b12
elvisstone <etas1409@gmail.com>
2024-06-04 18:10:23 +0300

    change a href link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,11 +27,7 @@ const Navbar = () => {
     <nav className="bg-navbarColor md:text-sm">
       <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
         <div className="flex items-center justify-between py-5 md:block ">
-          <a
-            href="https://clarusway.com/"
-            target="true"
-            className="flex items-center"
-          >
+          <a href="#" target="true" className="flex items-center">
             <span className="text-gray-700 hover:text-gray-900 font-medium">
               Elvis Stone
             </span>
